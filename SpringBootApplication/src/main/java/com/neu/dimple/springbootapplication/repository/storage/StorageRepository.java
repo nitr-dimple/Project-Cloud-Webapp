f9be11ee0cf820d9349a420b9cc6bad7cded7631
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2022-10-27 03:24:48 -0400

    changes for multipart file upload

--- SpringBootApplication/src/main/java/com/neu/dimple/springbootapplication/repository/storage/StorageRepository.java
+++ SpringBootApplication/src/main/java/com/neu/dimple/springbootapplication/repository/storage/StorageRepository.java
@@ -1,6 +1,7 @@
 package com.neu.dimple.springbootapplication.repository.storage;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,12 +33,17 @@ public class StorageRepository {
     @Autowired
     private AmazonS3 s3client;
 
-    public String uploadFile(MultipartFile file, String firstname, String lastname){
-        File fileObj = convertMultipartFileToFile(file);
+    public String uploadFile(MultipartFile file, String firstname, String lastname) {
+//        File fileObj = convertMultipartFileToFile(file);
+        try{
+            InputStream is=file.getInputStream();
+        }catch (IOException e){
+            logger.log(Level.INFO, "Error in convertiong inputstream " + e);
+        }
 
         String filename = firstname + "_" + lastname + "_" + file.getOriginalFilename() + "_" + System.currentTimeMillis();
-        s3client.putObject(new PutObjectRequest(bucketName, filename, fileObj));
-        fileObj.delete();
+        s3client.putObject(new PutObjectRequest(bucketName, filename, is,new ObjectMetadata()));
+//        fileObj.delete();
         logger.log(Level.INFO, "File: " + filename + " uploaded successfully");
         return filename;
     }
