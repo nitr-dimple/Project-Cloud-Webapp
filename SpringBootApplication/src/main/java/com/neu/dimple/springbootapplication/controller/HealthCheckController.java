8c0dadfcbe1e65bd2819e7d1053e43d8033d23ec
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2022-11-10 03:06:37 -0500

    change in health file

--- SpringBootApplication/src/main/java/com/neu/dimple/springbootapplication/controller/HealthCheckController.java
+++ SpringBootApplication/src/main/java/com/neu/dimple/springbootapplication/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.neu.dimple.springbootapplication.controller;
 
+import com.neu.dimple.springbootapplication.config.StatsdClient;
 import com.neu.dimple.springbootapplication.controller.accountcontroller.AccountController;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
@@ -11,10 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class HealthCheckController {
 
-    private static StatsDClient statsDClient = new NonBlockingStatsDClient("", "localhost", 8125);
+    private static StatsdClient statsDClient;
+
+    static {
+        try {
+            statsDClient = new StatsdClient("localhost", 8125);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
 
